d1a33ee606f40546cb46a99dd81fbbfd8d2d24d8
Michael Wright <wrigh517@gmail.com>
2015-09-22 15:04:45 -0400

    Add border around puzzle

--- app/src/main/java/edu/msu/wrigh517/puzzle/Puzzle.java
+++ app/src/main/java/edu/msu/wrigh517/puzzle/Puzzle.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
 import java.util.ArrayList;
@@ -12,6 +13,11 @@ import java.util.ArrayList;
  * This class represents our puzzle.
  */
 public class Puzzle {
+    /**
+     * Paint for drawing the border that the puzzle is in
+     */
+    //private Paint borderPaint;
+
     /**
      * Paint for filling the area the puzzle is in
      */
@@ -43,7 +49,8 @@ public class Puzzle {
         // Create paint for filling the area the puzzle will
         // be solved in.
         fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        fillPaint.setColor(0xffcccccc);
+
+
 
         // Load the solved puzzle image
         puzzleComplete = BitmapFactory.decodeResource(context.getResources(), R.drawable.sparty_done);
@@ -61,16 +68,24 @@ public class Puzzle {
         int minDim = wid < hit ? wid : hit;
 
         int puzzleSize = (int)(minDim * SCALE_IN_VIEW);
+        // Extend the size of the border past the size of the puzzle
+        int borderSize = 4;
+        int borderAdjustment = puzzleSize + borderSize;
 
         // Compute the margins so we center the puzzle
         int marginX = (wid - puzzleSize) / 2;
         int marginY = (hit - puzzleSize) / 2;
 
-        //
-        // Draw the outline of the puzzle
-        //
-
+        /*
+        Draw the border underneath the puzzle by setting green color,
+        drawing a larger rectangle, then setting to gray and drawing a
+        smaller one
+        */
+        fillPaint.setColor(Color.argb(255, 99, 226, 99));
+        canvas.drawRect(marginX - borderSize, marginY - borderSize, marginX + puzzleSize + borderSize, marginY + puzzleSize + borderSize, fillPaint);
+        fillPaint.setColor(0xffcccccc);
         canvas.drawRect(marginX, marginY, marginX + puzzleSize, marginY + puzzleSize, fillPaint);
+
 
         float scaleFactor = (float)puzzleSize / (float)puzzleComplete.getWidth();
 
